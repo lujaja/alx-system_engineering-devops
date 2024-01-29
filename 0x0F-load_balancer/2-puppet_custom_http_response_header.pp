05d0af44e2dfcd46eee3a4c1467094b3eb51aae1
lujaja <jarzcyber@gmail.com>
2024-01-29 23:51:01 +0300

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -17,8 +17,12 @@ exec {'redirect_me':
 	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
-
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+exec { 'command':
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  provider => shell,
 }
+
+service {'nginx':                                                             
+  ensure => running,                                                        
+  require => Package['nginx']                                               
+} 
