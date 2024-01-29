7458d8d33ada9c6182a1cb7c077807c33922bd3d
lujaja <jarzcyber@gmail.com>
2024-01-30 00:04:46 +0300

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -17,10 +17,20 @@ exec {'redirect_me':
 	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
-exec { 'command':
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  provider => shell,
+
+exec { 'add_header_to_nginx':
+  command     => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
+  path        => '/usr/bin:/bin', # Add the necessary path if required
+  provider    => 'shell',
+  refreshonly => true,             # Only run if notified
+  subscribe   => File['/etc/nginx/sites-available/default'], # Notify when the file changes
 }
+
+file { '/etc/nginx/sites-available/default':
+  ensure => present,
+  notify => Exec['add_header_to_nginx'], # Notify the exec resource when the file changes
+}
+
 
 service {'nginx':                                                             
   ensure => running,                                                        
