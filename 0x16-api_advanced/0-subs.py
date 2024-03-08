62c73e512a012337ad07b8b8142ff43bbf166265
lujaja <jarzcyber@gmail.com>
2024-03-08 14:38:41 +0300

    APIs advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""How many subs"""
-import requests
+"""
+number of subscribers for a given subreddit
+"""
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Returns number of subcribers for a given sub reddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return (0)
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-            "AppleWebKit/537.36 (KHTML, like Gecko)"
-            "Chrome/91.0.4472.124 Safari/537.36"
-        }
-    r = requests.get(
-            url,
-            headers=headers,
-            allow_redirects=False
-        )
-    if r.status_code != 200:
-        return (0)
-    r = r.json()
-    if 'data' in r:
-        return (r.get('data').get('subscribers'))
-    return (0)
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
